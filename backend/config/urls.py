789c7bab21a688b2d12a227205b82fea8e644b10
huynhbao123 <baobbbb2@gmail.com>
2026-03-21 05:09:17 +0700

    fix: serve media files in production

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,4 +10,5 @@ urlpatterns = [
     path('api/', include('criteria.urls')),
     path('api/', include('evidences.urls')),
     path('api/', include('content.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
